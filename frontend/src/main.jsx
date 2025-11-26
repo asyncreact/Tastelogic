ded6de0b9570091f1044d7b7f7601fcd806a765d
Mwcrt <miguelacostasrc@gmail.com>
2025-11-25 20:38:33 -0400

    autenticacion actualizada

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,31 +2,25 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// Importamos el nuevo Provider
-import { ThemeProvider } from './context/ThemeContext'; 
 import { AuthProvider } from './context/AuthContext';
 import { MenuProvider } from './context/MenuContext';
 import { OrderProvider } from './context/OrderContext';
 import { ReservationProvider } from './context/ReservationContext';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// Importa tus estilos globales aquí
-import './styles/App.css'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
-        <AuthProvider>
-          <MenuProvider>
-            <OrderProvider>
-              <ReservationProvider>
-                <App />
-              </ReservationProvider>
-            </OrderProvider>
-          </MenuProvider>
-        </AuthProvider>
-      </ThemeProvider>
+      <AuthProvider>
+        <MenuProvider>
+          <OrderProvider>
+            <ReservationProvider>
+              <App />
+            </ReservationProvider>
+          </OrderProvider>
+        </MenuProvider>
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
